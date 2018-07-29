d5ca8a02502cdb1a06c483c461c6c4ab2c99bcf0
adrian <adrian@192.168.0.38>
2018-07-29 20:52:26 +0200

    cleaning

--- metman/apps/metman-app/src/main/java/pl/metman/apps/sb/metman/controler/MetManRestControler.java
+++ metman/apps/metman-app/src/main/java/pl/metman/apps/sb/metman/controler/MetManRestControler.java
@@ -1,24 +1,18 @@
 package pl.metman.apps.sb.metman.controler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import pl.metman.modules.authorization.logic.AuthorizationBean;
 import pl.metman.modules.authorization.logic.AuthorizationService;
 
 @RestController
 public class MetManRestControler {
 	
+	@Autowired
 	private AuthorizationService auth;
 	
-	@Autowired
-	@Qualifier("auth")
-	public void setAuth() {
-		auth = new AuthorizationBean("Tom");
-	}
 	
 	@RequestMapping("/")
 	public String index(@RequestParam(value = "name", defaultValue = "mr nobody") String name) {
