0c0d8874b7ad7de95f22ebb3d97a4a18065e1134
adrian <adrian@192.168.0.38>
2018-07-29 11:34:07 +0200

    pierwszy modul

---
+++ metman/appclient/src/main/java/pl/metman/appclient/App.java
@@ -0,0 +1,13 @@
+package pl.metman.appclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
